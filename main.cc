89eb75577eae76683749e19f7203f80339b8a688
MarcKaise <73624858+MarcKaise@users.noreply.github.com>
2024-09-02 16:28:33 +0900

    Add greeting in Japanese

--- main.cc
+++ main.cc
@@ -1,8 +1,14 @@
 #include <iostream>
 
+#include "hellojp.h"
+
 using namespace std;
 
 int main(int argc, char *argv[]) {
     cout << "main" << endl;
+
+    HelloJp hj;
+    hj.hello();
+
     return 0;
 }
